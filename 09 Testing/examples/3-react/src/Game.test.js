552047f37a6a76a7f245ab775654307843a04cf7
Kasper Knop Rasmussen <kasperknop@gmail.com>
2023-04-10 17:25:10 +0200

    examples done

---
+++ 09 Testing/examples/3-react/src/Game.test.js
@@ -0,0 +1,15 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import Game from "./Game"
+
+test("X wins with diagonal line", () => {
+    const { container } = render(<Game />)
+    const squares = container.querySelectorAll(".square")
+
+    fireEvent.click(squares[0])
+    fireEvent.click(squares[1])
+    fireEvent.click(squares[4])
+    fireEvent.click(squares[2])
+    fireEvent.click(squares[8])
+
+    expect(screen.getByText("Winner: X")).toBeInTheDocument()
+})
